537cf12e9bbdff937b036dc4c64b50f811812405
John Joseph Bachir <j@jjb.cc>
2013-10-11 14:46:29 -0400

    generated new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4RspecRailsWrong::Application.config.secret_key_base = '7f44a6e98148d3edfb3b3c3a1a512093e67b5c57d212ccc107d9b31655f24790f74f7289767107eddac52bf05aa50b04e2802db5a3477005d3acbdef19793444'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4RspecRailsWrong::Application.config.session_store :cookie_store, key: '_rails_4_rspec_rails_wrong_session'
